c826b09be3e0f81654383d1f61dd49a4fc9c752d
ArcDepths-hash <ArcDepths@hotmail.com>
2026-06-22 20:25:48 +0200

    Add HTTP server for bot keep-alive

--- bot.js
+++ bot.js
@@ -1,1 +1,5 @@
-
+// --- RENDER FREE TIER KEEP-ALIVE ---
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is online!')).listen(process.env.PORT || 3000);
+console.log('🌐 Web listener initialized for Render free tier.');
+// ------------------------------------
